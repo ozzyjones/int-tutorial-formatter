0c0aa3755c403b319e1b47ff77abb66c29a7dfb1
Brian Jones <brian.jones@geotoolkit.net>
2018-03-28 15:43:06 -0500

    Capitalize the first word in every comment

--- src/TutorialFormatter.ts
+++ src/TutorialFormatter.ts
@@ -8,10 +8,20 @@ export class TutorialFormatter {
     public format(input: string): string {
         let output;
         output = this._putSpacesAfterCommentInitializations(input);
+        output = this._capitalizeFirstWordInComment(output);
         return output;
     }
 
     private _putSpacesAfterCommentInitializations(input: string): string {
         return input.replace(/\/\/(?=[^\s])/g, '// ');
+    }
+
+    private _capitalizeFirstWordInComment(input: string): string {
+        const re = /\/\/\s[a-z]/g
+        input.match(re).forEach((lowercaseCommentStart) => {
+            const lowerLetter = lowercaseCommentStart.charAt(3);
+            input = input.replace(lowercaseCommentStart, '// ' + lowerLetter.toUpperCase());
+        });
+        return input;
     }
 }

36f6cafed9120305c8dc4c5faef1bfb653bf2f2f
Brian Jones <brian.jones@geotoolkit.net>
2018-03-28 09:47:44 -0500

    Format Tutorials

---
+++ src/TutorialFormatter.ts
@@ -0,0 +1,11 @@
+
+export class TutorialFormatter {
+
+    /**
+     * Format Tutorial Text
+     * @returns {string} reformatted tutorial text
+     */
+    public format(input: string): string {
+        return input;
+    }
+}

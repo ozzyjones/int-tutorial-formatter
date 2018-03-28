897eb9b98256172eff830bd4719a7893abb1896c
Brian Jones <brian.jones@geotoolkit.net>
2018-03-28 15:51:36 -0500

    Install Grunt and TSLint

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function (grunt) {
+
+    // Project Config.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        tslint: {
+            options: {
+                configuration: 'tslint.json',
+                force: false,
+                fix: grunt.option('fix')
+            },
+            files: {
+                src: [
+                    'src/*.{ts,tsx}'
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-tslint');
+
+    grunt.registerTask('default', ['tslint']);
+    grunt.registerTask('lint', ['tslint']);
+
+    grunt.registerTask('help', function () {
+        var help = 
+        `
+    Help Menu
+    =========
+    grunt lint [--fix]        Lint Typescript
+        `
+        console.log(help);
+    });
+};

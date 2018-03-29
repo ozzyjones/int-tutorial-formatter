cdfaba4f40d966ed51d82e39479b18686fadf1b7
Brian Jones <brian.jones@geotoolkit.net>
2018-03-29 14:40:27 -0500

    HTML See More/Less Code Snippets

--- src/TutorialFormatter.ts
+++ src/TutorialFormatter.ts
@@ -11,6 +11,7 @@ export class TutorialFormatter {
         output = this._putSpacesAfterCommentInitializations(input);
         output = this._capitalizeFirstWordInComment(output);
         output = this._beautifyJavascript(output);
+        output = this._addCodeCollapseSnippets(output);
         return output;
     }
 
@@ -52,6 +53,11 @@ export class TutorialFormatter {
         return input;
     }
 
+    /**
+     * Beautify all JS snippets via the NPM package "js-beautify"
+     * 
+     * @param input all text to process
+     */
     private _beautifyJavascript(input: string): string {
         const regex = /<code\s.*?class=(["'])javascript\1.*?>([^<]*)<\/code>/g;
         let m = regex.exec(input);
@@ -75,6 +81,69 @@ export class TutorialFormatter {
                     input = input.replace(uglyCodeSnippet, prettyCodeSnippet.trim());
                 }
             });
+
+            m = regex.exec(input);
+        }
+        return input;
+    }
+
+    /**
+     * Add HTML code collapse snippets below every code block
+     * (Note: The JS definitions 'tutorialpagelogic' and 'jquery' still need to be added)
+     * 
+     * @param input all text to process
+     */
+    private _addCodeCollapseSnippets(input: string): string {
+        input = this._makeCodeBlocksCollapsible(input);
+        input = this._addSeeMoreLessCode(input);
+        return input;
+    }
+
+    private _makeCodeBlocksCollapsible(input: string): string {
+        // Matching this regex means that the "Show/Hide More Code..." snippets must be added
+        const regex = /<div\s.*?class="code">\s*<pre>\s*<code\s.*?class="javascript">/g;
+        let m = regex.exec(input);
+
+        while (m !== null) {
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+
+            const snippet = '<div class="code collapsed"><pre><code class="javascript">';
+            input = input.replace(m[0], snippet);
+
+            m = regex.exec(input);
+        }
+        return input;
+    }
+
+    private _addSeeMoreLessCode(input: string): string {
+        // Matching this regex means that the "Show/Hide More Code..." snippets must be added
+        const regex = /([^\n]*)<\/code>\s*<\/pre>\s*<\/div>(\s*)<\/div>/g;
+        let m = regex.exec(input);
+
+        while (m !== null) {
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+
+            let codeLeftPadding = m[1].length;  // Number of characters on the line before the code block
+            let divLeftPadding = m[2]           // Number of characters on the line before the div element
+            .replace('\n','')
+            .replace('\r','').length;
+
+            // Example:
+            // });</code></pre></div>
+            // ___<div ...See More...></div>
+            // ___<div ...See Less...></div>
+            // </div>
+
+            let snippet = m[1] + '</code></pre></div>\n' + 
+            ' '.repeat(codeLeftPadding) + '<div class="see-more-container" onclick="togglecode(event)"><a>See More Code...</a></div>\n' + 
+            ' '.repeat(codeLeftPadding) + '<div class="see-less-container hidden" onclick="togglecode(event)"><a>See Less Code...</a></div>\n' + 
+            ' '.repeat(divLeftPadding) + '</div>';
+
+            input = input.replace(m[0], snippet);
 
             m = regex.exec(input);
         }

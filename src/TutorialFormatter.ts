4bafdf4e08f1c332b6284cfc3085d0e8104b463b
Brian Jones <brian.jones@geotoolkit.net>
2018-04-06 14:34:32 -0500

    Use 'Cheerio' to parse HTML

--- src/TutorialFormatter.ts
+++ src/TutorialFormatter.ts
@@ -1,5 +1,5 @@
 
-import htmlparser = require('htmlparser2');
+import cheerio = require('cheerio');
 import titlecase = require('titlecase');
 import { CodeFormatter } from './CodeFormatter';
 
@@ -21,17 +21,8 @@ export class TutorialFormatter {
             output = this._renameReferencesLink(output);
         }
 
-        const codeFormatter = new CodeFormatter();
-        const snippets = this._getCodeSnippets(output);
-        snippets.forEach((uglyCodeSnippet) => {
-            let prettyCodeSnippet;
-            try {
-                prettyCodeSnippet = codeFormatter.format(uglyCodeSnippet);
-            } catch (error) {
-                throw error;
-            }
-            output = output.replace(uglyCodeSnippet, prettyCodeSnippet);
-        });
+        // Format Code Snippets
+        output = this._editCodeSnippets(output);
 
         return output;
     }
@@ -43,84 +34,55 @@ export class TutorialFormatter {
      * @param input all text to process
      */
     private _titleCaseHeading(input: string): string {
-        const m = input.match(/<h2>([^<]+)<\/h2>/);
-        if (m !== null) {
-            const title = titlecase(m[1]);
-            input = input.replace(m[0], `<h2>${title}</h2>`);
-        }
-        return input;
+        const $ = cheerio.load(input, {'decodeEntities': false});
+        $('h2').text(function() {
+            return titlecase($(this).text());
+        });
+        return $.html();
     }
 
     /**
-     * Title case the table of contents
+     * Title case the table of contents and sub-headers
      *
      * @param input all text to process
      */
     private _titleCaseSubHeadings(input: string): string {
-        const tocRegex = /<li><a\s+href="#([^"]+)">([^<]+)<\/a><\/li>/g;
-        let tocMatches = tocRegex.exec(input); // Table of Contents
+        const $ = cheerio.load(input, {'decodeEntities': false});
 
-        while (tocMatches !== null) {
-            if (tocMatches.index === tocRegex.lastIndex) {
-                tocRegex.lastIndex++;
-            }
+        // Table of Contents
+        $('li>a').text(function() {
+            return titlecase($(this).text());
+        });
 
-            const id = tocMatches[1];
-            const title = titlecase(tocMatches[2]);
-            input = input.replace(tocMatches[0], `<li><a href="#${id}">${title}</a></li>`);
-            tocMatches = tocRegex.exec(input);
-        }
+        // Sub Headers
+        $('h3>a').text(function() {
+            return titlecase($(this).text());
+        });
 
-        const subheaderRegex = /<h3><a\s+id="([^"]+)">([^<]+)<\/a><\/h3>/g;
-        let subheaderMatches = subheaderRegex.exec(input);
-        while (subheaderMatches !== null) {
-            if (subheaderMatches.index === subheaderRegex.lastIndex) {
-                subheaderRegex.lastIndex++;
-            }
-
-            const id = subheaderMatches[1];
-            const title = titlecase(subheaderMatches[2]);
-            input = input.replace(subheaderMatches[0], `<h3><a id="${id}">${title}</a></h3>`);
-            subheaderMatches = subheaderRegex.exec(input);
-        }
-
-        return input;
+        return $.html();
     }
 
     /**
      * Extract JS Code snippets from the HTML tutorial
      *
      * @param input HTML Tutorial
-     * @returns {string[]} Array of code snippets
+     * @returns {string} Tutorial with formatted code snippets
+     * @throws {ESLintMessagesError}
      */
-    private _getCodeSnippets(input: string): string[] {
-        const snippets = [];
-
-        let code = '';
-        let isInOpenTag = false;
-        const parser = new htmlparser.Parser({
-            'onclosetag': (tagname) => {
-                if (tagname === 'code' && isInOpenTag) {
-                    isInOpenTag = false;
-                    snippets.push(code);
-                    code = '';
-                }
-            },
-            'onopentag': (tagname, attribs) => {
-                if (tagname === 'code' && attribs.class === 'javascript') {
-                    isInOpenTag = true;
-                }
-            },
-            'ontext': (text) => {
-                if (isInOpenTag) {
-                    code += (text !== undefined) ? text : '';
-                }
+    private _editCodeSnippets(input: string): string {
+        const codeFormatter = new CodeFormatter();
+        const $ = cheerio.load(input, {'decodeEntities': false});
+        $('div>pre>code.javascript').each(function(index) {
+            let prettyCodeSnippet;
+            const uglyCodeSnippet = $(this).text();
+            try {
+                prettyCodeSnippet = codeFormatter.format(uglyCodeSnippet);
+            } catch (error) {
+                throw error;
             }
-        }, {'decodeEntities': false});
-        parser.write(input);
-        parser.end();
-
-        return snippets;
+            $(this).text(prettyCodeSnippet);
+        });
+        return $.html();
     }
 
     /**
@@ -135,58 +97,40 @@ export class TutorialFormatter {
         return input;
     }
 
+    /**
+     * Add 'collapsed' class to javascript code blocks
+     *
+     * @example <div class="code collapsed"><pre><code class="javascript">
+     * @param input
+     */
     private _makeCodeBlocksCollapsible(input: string): string {
-        // Matching this regex means that the "Show/Hide More Code..." snippets must be added
-        const regex = /<div\s.*?class="code">\s*<pre>\s*<code\s.*?class="javascript">/g;
-        let m = regex.exec(input);
-
-        while (m !== null) {
-            if (m.index === regex.lastIndex) {
-                regex.lastIndex++;
-            }
-
-            const snippet = '<div class="code collapsed"><pre><code class="javascript">';
-            input = input.replace(m[0], snippet);
-
-            m = regex.exec(input);
-        }
-        return input;
+        const $ = cheerio.load(input, {'decodeEntities': false});
+        $('div.code>pre>code.javascript').parent().parent().addClass('collapsed');
+        return $.html();
     }
 
+    /**
+     * Add "Show More/Less Code..." snipppets
+     *
+     * @example
+     *   });</code></pre></div>
+     *      <div ...See More...></div>
+     *      <div ...See Less...></div>
+     *   </div>
+     * @param input
+     */
     private _addSeeMoreLessCode(input: string): string {
-        // Matching this regex means that the "Show/Hide More Code..." snippets must be added
-        const regex = /<code class="javascript">([^>]*)<\/code>\s*<\/pre>\s*<\/div>(\s*)<\/div>/g;
-        let m = regex.exec(input);
-
-        while (m !== null) {
-            if (m.index === regex.lastIndex) {
-                regex.lastIndex++;
-            }
-
-            // let codeLeftPadding = m[1].length;  // Number of characters on the line before the code block
-            const divLeftPadding = m[2]           // Number of characters on the line before the div element
-            .replace('\n', '')
-            .replace('\r', '').length;
-            const codeLeftPadding = divLeftPadding + 4;   // 4 spaces indented from the closing div
-
-            // Example:
-            // });</code></pre></div>
-            // ___<div ...See More...></div>
-            // ___<div ...See Less...></div>
-            // </div>
-
-            const snippet = m[1] + '</code></pre></div>\n' +
-            ' '.repeat(codeLeftPadding) +
-            '<div class="see-more-container" onclick="togglecode(event)"><a>See More Code...</a></div>\n' +
-            ' '.repeat(codeLeftPadding) +
-            '<div class="see-less-container hidden" onclick="togglecode(event)"><a>See Less Code...</a></div>\n' +
-            ' '.repeat(divLeftPadding) + '</div>';
-
-            input = input.replace(m[0], snippet);
-
-            m = regex.exec(input);
+        const $ = cheerio.load(input, {'decodeEntities': false});
+        const $row = $('div.code>pre>code.javascript').parent().parent().parent();
+        if ($row.children().length === 1) {
+            $row.append(
+                '<div class="see-more-container" onclick="togglecode(event)"><a>See More Code...</a></div>'
+            );
+            $row.append(
+                '<div class="see-less-container hidden" onclick="togglecode(event)"><a>See Less Code...</a></div>'
+            );
         }
-        return input;
+        return $.html();
     }
 
     /**
@@ -195,15 +139,8 @@ export class TutorialFormatter {
      * @param input all text to process
      */
     private _renameReferencesLink(input: string): string {
-        const regex = /<li>\s*<a\s+.*?href="#References">(.*)<\/a><\/li>/;
-        const m = input.match(regex);
-        if (m !== null) {
-            const link = m[0];
-            const linkName = m[1];
-            if (linkName !== 'References') {
-                input = input.replace(link, '<li><a href="#References">References</a></li>');
-            }
-        }
-        return input;
+        const $ = cheerio.load(input, {'decodeEntities': false});
+        $('li>a[href*="References"]').text('References');
+        return $.html();
     }
 }
--- src/test/TutorialFormatter.test.ts
+++ src/test/TutorialFormatter.test.ts
@@ -7,32 +7,43 @@ const formatter = new TutorialFormatter();
 describe('Tutorial Formatter', () => {
 
     it('title case header', () => {
-        const before = '<h2>Real-time Demo</h2>';
-        const after = '<h2>Real-Time Demo</h2>';
+        const before = '<html><head></head><body><h2>Real-time Demo</h2></body></html>';
+        const after = '<html><head></head><body><h2>Real-Time Demo</h2></body></html>';
         expect(formatter.format(before)).to.equal(after);
     });
 
     it('title case sub-header in table of contents', () => {
-        const before = '<li><a href="#Depth based realtime">Depth-based Real-time</a></li>';
-        const after = '<li><a href="#Depth based realtime">Depth-Based Real-Time</a></li>';
+        const before = '<html><head></head><body><li><a href="#Depth based realtime">Depth-based Real-time</a></li></body></html>';
+        const after = '<html><head></head><body><li><a href="#Depth based realtime">Depth-Based Real-Time</a></li></body></html>';
         expect(formatter.format(before)).to.equal(after);
     });
 
     it('title case sub-header in body', () => {
-        const before = '<h3><a id="Depth based realtime">Depth-based Real-time</a></h3>';
-        const after = '<h3><a id="Depth based realtime">Depth-Based Real-Time</a></h3>';
+        const before = '<html><head></head><body><h3><a id="Depth based realtime">Depth-based Real-time</a></h3></body></html>';
+        const after = '<html><head></head><body><h3><a id="Depth based realtime">Depth-Based Real-Time</a></h3></body></html>';
         expect(formatter.format(before)).to.equal(after);
     });
 
     it('collapsed javascript code block', () => {
-        const before = '<div class="code"><pre><code class="javascript">var a = 0;</code></pre></div>';
-        const after = '<div class="code collapsed"><pre><code class="javascript">var a = 0;</code></pre></div>';
+        const before = '<html><head></head><body><div class="code"><pre><code class="javascript">var a = 0;</code></pre></div></body></html>';
+        const after = '<html><head></head><body><div class="code collapsed"><pre><code class="javascript">var a = 0;</code></pre></div><div class="see-more-container" onclick="togglecode(event)"><a>See More Code...</a></div><div class="see-less-container hidden" onclick="togglecode(event)"><a>See Less Code...</a></div></body></html>';
         expect(formatter.format(before)).to.equal(after);
     });
 
+    it('collapsed javascript code block - already collapsable', () => {
+        const s = '<html><head></head><body><div class="code collapsed"><pre><code class="javascript">var a = 0;</code></pre></div><div class="see-more-container" onclick="togglecode(event)"><a>See More Code...</a></div><div class="see-less-container hidden" onclick="togglecode(event)"><a>See Less Code...</a></div></body></html>';
+        expect(formatter.format(s)).to.equal(s);
+    });
+
     it('do not make non-javascript code block collapsible', () => {
-        const before = '<div class="code"><pre><code>npm install jsonschema</code></pre></div>';
-        const after = '<div class="code"><pre><code>npm install jsonschema</code></pre></div>';
+        const before = '<html><head></head><body><div class="code"><pre><code>npm install jsonschema</code></pre></div></body></html>';
+        const after = '<html><head></head><body><div class="code"><pre><code>npm install jsonschema</code></pre></div></body></html>';
+        expect(formatter.format(before)).to.equal(after);
+    });
+
+    it('rename references link', () => {
+        const before = '<html><head></head><body><li><a href="References">Reference Links</a></li></body></html>';
+        const after =  '<html><head></head><body><li><a href="References">References</a></li></body></html>';
         expect(formatter.format(before)).to.equal(after);
     });
 });
